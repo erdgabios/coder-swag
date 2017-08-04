f3c836bb65f64a7ca89672441e1d9452d5849554
Gabriel Erdelyi <erdgabios@gmail.com>
2017-08-04 17:48:22 +0200

    Collection views, grid layouts

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by G on 04/08/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}

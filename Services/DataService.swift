07cc302b9072b665fb219e753012d70fa7619c07
Gabriel Erdelyi <erdgabios@gmail.com>
2017-08-04 16:44:21 +0200

    Tableviews, delegate, and datasource

---
+++ Services/DataService.swift
@@ -0,0 +1,38 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by G on 04/08/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> [Category] {
+        return categories
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ coder-swag/Model/Category.swift
@@ -0,0 +1,26 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by G on 04/08/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
+
+
+
+
+
+
+

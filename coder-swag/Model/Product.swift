5b69ddd711bd15afcad1c65bf6ff4db975194971
Gabriel Erdelyi <erdgabios@gmail.com>
2017-08-04 18:34:20 +0200

    Working with data models

---
+++ coder-swag/Model/Product.swift
@@ -0,0 +1,22 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by G on 04/08/2017.
+//  Copyright © 2017 erdgabios. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    private(set) public var title: String
+    private (set) public var price: String
+    private (set) public var imageName: String
+    
+    init(title:String, price: String, imageName: String) {
+        self.title = title
+        self.price = price
+        self.imageName = imageName
+    }
+    
+}
